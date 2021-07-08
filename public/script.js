ebb95fa7d4f0e23efd551946332789fe116a9905
Shristi <2111shristi@gmail.com>
2021-07-08 18:51:17 +0530

    updated UI

--- public/script.js
+++ public/script.js
@@ -32,7 +32,7 @@ const copyToClipboard = () =>{
 const options = {
   bottom: '32px', // default: '32px'
   right: 'unset', // default: '32px'
-  left: '32px', // default: 'unset'
+  left: '200px', // default: 'unset'
   time: '0.3s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
   backgroundColor: '#fff',  // default: '#fff'
